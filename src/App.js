fdaedd9df644d031d92d10c25f9efc8f0caaffc4
nidhikaranam <219x1a2820@gprec.ac.in>
2025-06-04 15:44:08 +0530

    i changed in app.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Testimonial from "./components/tesimonial/Testimonial";
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+      <div className="w-full" ></div>
         <Navbar />
       <div className="max-w-screen-xl mx-auto">
         <Banner />
